ac3c1aab722c5763c6089edfa7d9c3700f4db3ce
0x00-pl <0x00.pl@gmail.com>
2019-03-02 11:51:52 +0000

    modified:   src/mipi.h

--- src/mipi.h
+++ src/mipi.h
@@ -3,56 +3,44 @@
 
 #define STRUCT_PACKED __attribute__((__packed__))
 
-typedef struct STRUCT_PACKED _mipi_dpi_t{
-    uint8_t SoT;
-    uint8_t payload;
-    uint8_t Eot;
+typedef struct STRUCT_PACKED _mipi_dpi_t {
+	uint8_t SoT;
+	uint8_t payload;
+	uint8_t Eot;
 } mipi_dpi_t;
 
-
 typedef enum {
-    MIPI_SYNC_V_START=0x01,
-    MIPI_SYNC_V_END=0x11,
-    MIPI_SYNC_H_START=0x21,
-    MIPI_SYNC_H_END=0x31,
-    MIPI_COMPRESSION_MODE=0x07
+	MIPI_SYNC_V_START = 0x01,
+	MIPI_SYNC_V_END = 0x11,
+	MIPI_SYNC_H_START = 0x21,
+	MIPI_SYNC_H_END = 0x31,
+	MIPI_COMPRESSION_MODE = 0x07
 } data_type_e;
 
-typedef struct STRUCT_PACKED _mipi_data_id_t{
-    data_type_e data_type:6;
-    uint8_t virtual_channel_indentifier:2;
+typedef struct STRUCT_PACKED _mipi_data_id_t {
+	data_type_e data_type : 6;
+	uint8_t virtual_channel_indentifier : 2;
 } mipi_data_id_t;
 
-
-typedef struct STRUCT_PACKED _mipi_packet_header_t{
-    mipi_data_id_t data_id;
-    uint16_t word_count;
-    uint8_t ecc;
+typedef struct STRUCT_PACKED _mipi_packet_header_t {
+	mipi_data_id_t data_id;
+	uint16_t word_count;
+	uint8_t ecc;
 } mipi_packet_header_t;
 
-typedef struct STRUCT_PACKED _mipi_short_packet_t{
-    mipi_data_id_t data_id;
-    uint8_t data0;
-    uint8_t data1;
-    uint8_t ecc;
+typedef struct STRUCT_PACKED _mipi_short_packet_t {
+	mipi_data_id_t data_id;
+	uint8_t data0;
+	uint8_t data1;
+	uint8_t ecc;
 } mipi_short_packet_t;
 
-
-#define mipi_long_packet_checksum(packet) (*(int16_t*)&((packet).payload[(packet).packet_header.word_count]))
-typedef struct STRUCT_PACKED _mipi_long_packet_t{
-    mipi_packet_header_t packet_header;
-    /// payload size is packet_header.word_count
-    uint8_t payload[];
-    // checksum is at end of packet
-    // uint16_t checksum;
+#define mipi_long_packet_checksum(packet)                                      \
+	(*(int16_t *)&((packet).payload[(packet).packet_header.word_count]))
+typedef struct STRUCT_PACKED _mipi_long_packet_t {
+	mipi_packet_header_t packet_header;
+	/// payload size is packet_header.word_count
+	uint8_t payload[];
+	// checksum is at end of packet
+	// uint16_t checksum;
 } mipi_long_packet_t;
-
-
-
-
-
-
-
-
-
-
