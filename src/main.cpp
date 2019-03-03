b7d50eb46f05dc9045679aec5739c22fb87d6d2e
0x00-pl <0x00.pl@gmail.com>
2019-03-03 11:52:10 +0000

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "mipi.h"
 
-int main(int argc, char **argv) {
-    
-    mipi_long_packet_t lp;
-    mipi_long_packet_checksum(lp) = 10;
-    
-    std::cout << "Hello, world!" << sizeof(mipi_long_packet_t) <<  mipi_long_packet_checksum(lp) << lp.payload[0] << std::endl;
-    return 0;
+int main(int argc, char **argv)
+{
+	mipi_long_packet_t lp;
+	mipi_long_packet_checksum(lp) = 10;
+
+	std::cout << "Hello, world!" << sizeof(mipi_long_packet_t)
+		  << mipi_long_packet_checksum(lp) << lp.payload[0]
+		  << std::endl;
+	return 0;
 }
