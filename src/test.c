06dedf32849e277b5aa7adf768d050445b9dec6d
0x00-pl <0x00.pl@gmail.com>
2019-03-26 06:45:22 +0000

    modified:   src/test.c

--- src/test.c
+++ src/test.c
@@ -1,7 +1,5 @@
 #include <stdint.h>
 
-
-
 typedef struct _trans_block_16_t {
     uint16_t line0[4];
     uint16_t line1[4];
@@ -9,7 +7,6 @@ typedef struct _trans_block_16_t {
     uint16_t line3[4];
 } transs_block_16_t;
 
-#define AS_UINT64(addr) (*(uint64_t*)(addr))
 
 static inline void swap16(uint16_t& a, uint16_t& b){
     uint16_t t = a;
@@ -17,12 +14,14 @@ static inline void swap16(uint16_t& a, uint16_t& b){
     b = t;
 } 
 
-void transpose_block_16(uint64_t* src, uint64_t* dst, uint32_t src_w, uint32_t dst_w){
+#define AS_UINT64(addr) (*(uint64_t*)(addr))
+
+void transpose_block_16(uint64_t* src, uint64_t* dst, uint32_t src_skip, uint32_t dst_skip){
     transs_block_16_t tmp;
-    AS_UINT64(&tmp.line0) = src[0];
-    AS_UINT64(&tmp.line1) = src[src_w/4];
-    AS_UINT64(&tmp.line2) = src[2*src_w/4];
-    AS_UINT64(&tmp.line3) = src[3*src_w/4];
+    AS_UINT64(&tmp.line0) = *src; src+=src_skip;
+    AS_UINT64(&tmp.line1) = *src; src+=src_skip;
+    AS_UINT64(&tmp.line2) = *src; src+=src_skip;
+    AS_UINT64(&tmp.line3) = *src;
     
     swap16(tmp.line0[1], tmp.line1[0]);
     swap16(tmp.line2[3], tmp.line3[2]);
@@ -31,8 +30,8 @@ void transpose_block_16(uint64_t* src, uint64_t* dst, uint32_t src_w, uint32_t d
     swap16(tmp.line0[3], tmp.line3[0]);
     swap16(tmp.line1[2], tmp.line2[1]);
     
-    dst[0] = AS_UINT64(&tmp.line0);
-    dst[dst_w/4] = AS_UINT64(&tmp.line1);
-    dst[2*dst_w/4] = AS_UINT64(&tmp.line2);
-    dst[3*dst_w/4] = AS_UINT64(&tmp.line3);
+    *dst = AS_UINT64(&tmp.line0); dst+=dst_skip;
+    *dst = AS_UINT64(&tmp.line1); dst+=dst_skip;
+    *dst = AS_UINT64(&tmp.line2); dst+=dst_skip;
+    *dst = AS_UINT64(&tmp.line3);
 }
