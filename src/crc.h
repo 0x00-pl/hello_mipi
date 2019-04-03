2a34c6be6b184d734ea010bee23ba23ca2529b1c
0x00-pl <0x00.pl@gmail.com>
2019-04-03 13:34:52 +0100

    modified:   src/crc.h

--- src/crc.h
+++ src/crc.h
@@ -1,5 +1,6 @@
 #pragma once
 
-static unsigned short gsCRC16GenerationCode = 0x8408;
 unsigned short CalculateCRC16(unsigned char *pcDataStream,
 			      unsigned short sNumberOfDataBytes);
+
+unsigned short CalculateCRC12(unsigned char *pcDataStream);
